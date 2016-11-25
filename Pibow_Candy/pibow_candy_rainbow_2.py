72b5d6a3a07e19e249ebc3d9ff676ca54fc0896b
Paul Ryan <ShineTop@users.noreply.github.com>
2016-11-25 13:05:40 -0600

    Update pibow_candy_rainbow_2.py

--- Pibow_Candy/pibow_candy_rainbow_2.py
+++ Pibow_Candy/pibow_candy_rainbow_2.py
@@ -1,23 +1,19 @@
-#!/usr/bin/python
 ################################################################
 #                 Pibow Candy Rainbow 2                        #
 ################################################################
 # Description:                                                 #
-# This is a modification of my Moving Rainbow program. I just  #
-# changed the colors to pastels to match the Pibow Candy case. #
+# It's like Pibow Canday Rainbow except that it moves in the   #
+# other direction.                                             #
 #                                                              #
 # Author: Paul Ryan                                            #
 #                                                              #
 ################################################################
-
+#!/usr/bin/python
 import unicornhat, signal, time, random
 
 unicornhat.set_layout(unicornhat.PHAT)
 unicornhat.brightness(0.5)
 unicornhat.rotation(180)
-
-def get_color():
-    return int(random.randint(1, 255))
 
 R = (255, 105, 97)
 O = (255, 179, 71)
@@ -29,125 +25,132 @@ V = (203, 153, 201)
 W = (244, 154, 194)
 off = (0, 0, 0)
 
-rainbow0 = [
-    [R, O, Y, G, B, I, V, W], 
-    [R, O, Y, G, B, I, V, W], 
-    [R, O, Y, G, B, I, V, W], 
-    [R, O, Y, G, B, I, V, W], 
-    # Don't use the 4 rows below for UnicornPHAT, use only for UnicornHAT
-    [off, off, off, off,off, off, off, off], 
-    [off, off, off, off, off, off, off, off], 
-    [off, off, off, off, off, off, off, off], 
-    [off, off, off, off, off, off, off, off]
-]
+def pibow_candy_rainbow_2():
 
-rainbow1 = [
-    [W, R, O, Y, G, B, I, V], 
-    [W, R, O, Y, G, B, I, V], 
-    [W, R, O, Y, G, B, I, V], 
-    [W, R, O, Y, G, B, I, V], 
-    # Don't use the 4 rows below for UnicornPHAT, use only for UnicornHAT
-    [off, off, off, off,off, off, off, off], 
-    [off, off, off, off, off, off, off, off], 
-    [off, off, off, off, off, off, off, off], 
-    [off, off, off, off, off, off, off, off]
-]
+	rainbow0 = [
+		[R, O, Y, G, B, I, V, W], 
+		[R, O, Y, G, B, I, V, W], 
+		[R, O, Y, G, B, I, V, W], 
+		[R, O, Y, G, B, I, V, W], 
+		# Don't use the 4 rows below for UnicornPHAT, use only for UnicornHAT
+		[off, off, off, off,off, off, off, off], 
+		[off, off, off, off, off, off, off, off], 
+		[off, off, off, off, off, off, off, off], 
+		[off, off, off, off, off, off, off, off]
+	]
 
-rainbow2 = [
-    [V, W, R, O, Y, G, B, I], 
-    [V, W, R, O, Y, G, B, I], 
-    [V, W, R, O, Y, G, B, I], 
-    [V, W, R, O, Y, G, B, I], 
-    # Don't use the 4 rows below for UnicornPHAT, use only for UnicornHAT
-    [off, off, off, off,off, off, off, off], 
-    [off, off, off, off, off, off, off, off], 
-    [off, off, off, off, off, off, off, off], 
-    [off, off, off, off, off, off, off, off]
-]
+	rainbow1 = [
+		[W, R, O, Y, G, B, I, V], 
+		[W, R, O, Y, G, B, I, V], 
+		[W, R, O, Y, G, B, I, V], 
+		[W, R, O, Y, G, B, I, V], 
+		# Don't use the 4 rows below for UnicornPHAT, use only for UnicornHAT
+		[off, off, off, off,off, off, off, off], 
+		[off, off, off, off, off, off, off, off], 
+		[off, off, off, off, off, off, off, off], 
+		[off, off, off, off, off, off, off, off]
+	]
 
-rainbow3 = [
-    [I, V, W, R, O, Y, G, B], 
-    [I, V, W, R, O, Y, G, B], 
-    [I, V, W, R, O, Y, G, B], 
-    [I, V, W, R, O, Y, G, B],
-    # Don't use the 4 rows below for UnicornPHAT, use only for UnicornHAT
-    [off, off, off, off,off, off, off, off], 
-    [off, off, off, off, off, off, off, off], 
-    [off, off, off, off, off, off, off, off], 
-    [off, off, off, off, off, off, off, off]
-]
+	rainbow2 = [
+		[V, W, R, O, Y, G, B, I], 
+		[V, W, R, O, Y, G, B, I], 
+		[V, W, R, O, Y, G, B, I], 
+		[V, W, R, O, Y, G, B, I], 
+		# Don't use the 4 rows below for UnicornPHAT, use only for UnicornHAT
+		[off, off, off, off,off, off, off, off], 
+		[off, off, off, off, off, off, off, off], 
+		[off, off, off, off, off, off, off, off], 
+		[off, off, off, off, off, off, off, off]
+	]
 
-rainbow4 = [
-    [B, I, V, W, R, O, Y, G], 
-    [B, I, V, W, R, O, Y, G], 
-    [B, I, V, W, R, O, Y, G], 
-    [B, I, V, W, R, O, Y, G], 
-    # Don't use the 4 rows below for UnicornPHAT, use only for UnicornHAT
-    [off, off, off, off,off, off, off, off], 
-    [off, off, off, off, off, off, off, off], 
-    [off, off, off, off, off, off, off, off], 
-    [off, off, off, off, off, off, off, off]
-]
+	rainbow3 = [
+		[I, V, W, R, O, Y, G, B], 
+		[I, V, W, R, O, Y, G, B], 
+		[I, V, W, R, O, Y, G, B], 
+		[I, V, W, R, O, Y, G, B],
+		# Don't use the 4 rows below for UnicornPHAT, use only for UnicornHAT
+		[off, off, off, off,off, off, off, off], 
+		[off, off, off, off, off, off, off, off], 
+		[off, off, off, off, off, off, off, off], 
+		[off, off, off, off, off, off, off, off]
+	]
 
-rainbow5 = [
-    [G, B, I, V, W, R, O, Y], 
-    [G, B, I, V, W, R, O, Y], 
-    [G, B, I, V, W, R, O, Y], 
-    [G, B, I, V, W, R, O, Y], 
-    # Don't use the 4 rows below for UnicornPHAT, use only for UnicornHAT
-    [off, off, off, off,off, off, off, off], 
-    [off, off, off, off, off, off, off, off], 
-    [off, off, off, off, off, off, off, off], 
-    [off, off, off, off, off, off, off, off]
-]
+	rainbow4 = [
+		[B, I, V, W, R, O, Y, G], 
+		[B, I, V, W, R, O, Y, G], 
+		[B, I, V, W, R, O, Y, G], 
+		[B, I, V, W, R, O, Y, G], 
+		# Don't use the 4 rows below for UnicornPHAT, use only for UnicornHAT
+		[off, off, off, off,off, off, off, off], 
+		[off, off, off, off, off, off, off, off], 
+		[off, off, off, off, off, off, off, off], 
+		[off, off, off, off, off, off, off, off]
+	]
 
-rainbow6 = [
-    [Y, G, B, I, V, W, R, O],
-    [Y, G, B, I, V, W, R, O], 
-    [Y, G, B, I, V, W, R, O], 
-    [Y, G, B, I, V, W, R, O], 
-    # Don't use the 4 rows below for UnicornPHAT, use only for UnicornHAT
-    [off, off, off, off,off, off, off, off], 
-    [off, off, off, off, off, off, off, off], 
-    [off, off, off, off, off, off, off, off], 
-    [off, off, off, off, off, off, off, off]
-]
+	rainbow5 = [
+		[G, B, I, V, W, R, O, Y], 
+		[G, B, I, V, W, R, O, Y], 
+		[G, B, I, V, W, R, O, Y], 
+		[G, B, I, V, W, R, O, Y], 
+		# Don't use the 4 rows below for UnicornPHAT, use only for UnicornHAT
+		[off, off, off, off,off, off, off, off], 
+		[off, off, off, off, off, off, off, off], 
+		[off, off, off, off, off, off, off, off], 
+		[off, off, off, off, off, off, off, off]
+	]
 
-rainbow7 = [
-    [O, Y, G, B, I, V, W, R], 
-    [O, Y, G, B, I, V, W, R], 
-    [O, Y, G, B, I, V, W, R], 
-    [O, Y, G, B, I, V, W, R], 
-    # Don't use the 4 rows below for UnicornPHAT, use only for UnicornHAT
-    [off, off, off, off,off, off, off, off], 
-    [off, off, off, off, off, off, off, off], 
-    [off, off, off, off, off, off, off, off], 
-    [off, off, off, off, off, off, off, off]
-]
+	rainbow6 = [
+		[Y, G, B, I, V, W, R, O],
+		[Y, G, B, I, V, W, R, O], 
+		[Y, G, B, I, V, W, R, O], 
+		[Y, G, B, I, V, W, R, O], 
+		# Don't use the 4 rows below for UnicornPHAT, use only for UnicornHAT
+		[off, off, off, off,off, off, off, off], 
+		[off, off, off, off, off, off, off, off], 
+		[off, off, off, off, off, off, off, off], 
+		[off, off, off, off, off, off, off, off]
+	]
 
+	rainbow7 = [
+		[O, Y, G, B, I, V, W, R], 
+		[O, Y, G, B, I, V, W, R], 
+		[O, Y, G, B, I, V, W, R], 
+		[O, Y, G, B, I, V, W, R], 
+		# Don't use the 4 rows below for UnicornPHAT, use only for UnicornHAT
+		[off, off, off, off,off, off, off, off], 
+		[off, off, off, off, off, off, off, off], 
+		[off, off, off, off, off, off, off, off], 
+		[off, off, off, off, off, off, off, off]
+	]
 
-while True:
-    unicornhat.set_pixels(rainbow0)
-    unicornhat.show()
-    time.sleep(0.5)
-    unicornhat.set_pixels(rainbow1)
-    unicornhat.show()
-    time.sleep(0.5)
-    unicornhat.set_pixels(rainbow2)
-    unicornhat.show()
-    time.sleep(0.5)
-    unicornhat.set_pixels(rainbow3)
-    unicornhat.show()
-    time.sleep(0.5)
-    unicornhat.set_pixels(rainbow4)
-    unicornhat.show()
-    time.sleep(0.5)
-    unicornhat.set_pixels(rainbow5)
-    unicornhat.show()
-    time.sleep(0.5)
-    unicornhat.set_pixels(rainbow6)
-    unicornhat.show()
-    time.sleep(0.5)
-    unicornhat.set_pixels(rainbow7)
-    unicornhat.show()
-    time.sleep(0.5)
+	unicornhat.set_pixels(rainbow0)
+	unicornhat.show()
+	time.sleep(0.5)
+	unicornhat.set_pixels(rainbow1)
+	unicornhat.show()
+	time.sleep(0.5)
+	unicornhat.set_pixels(rainbow2)
+	unicornhat.show()
+	time.sleep(0.5)
+	unicornhat.set_pixels(rainbow3)
+	unicornhat.show()
+	time.sleep(0.5)
+	unicornhat.set_pixels(rainbow4)
+	unicornhat.show()
+	time.sleep(0.5)
+	unicornhat.set_pixels(rainbow5)
+	unicornhat.show()
+	time.sleep(0.5)
+	unicornhat.set_pixels(rainbow6)
+	unicornhat.show()
+	time.sleep(0.5)
+	unicornhat.set_pixels(rainbow7)
+	unicornhat.show()
+	time.sleep(0.5)
+
+def main():
+	while True:
+		pibow_candy_rainbow_2()
+
+if __name__ == '__main__':
+	main()
