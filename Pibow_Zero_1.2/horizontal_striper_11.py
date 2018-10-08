72a33b731a3ede643201ed9600800b662a10135b
Paul <13591438+Breakfast-for-Pigeons@users.noreply.github.com>
2018-10-08 11:28:11 -0500

    Add files via upload

---
+++ Pibow_Zero_1.2/horizontal_striper_11.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+"""
+Horizontal Striper 11 - Pibow Zero 1.2
+
+With the Raspberry Pi oriented with the GPIO pins at the top, this
+program stripes from the bottom to the top and from left to right.
+
+This is exactly the same at Horizontal Striper 3 except the color order
+is reversed.
+
+....................
+
+Functions:
+- horizontal_striper_11: Gets x and y coordinates and sends them to the
+      striping function
+
+....................
+
+Author: Paul Ryan
+
+This program was written on a Raspberry Pi using the Geany IDE.
+"""
+########################################################################
+#                          Import modules                              #
+########################################################################
+
+from bfp_unicornphat import print_header
+from bfp_unicornphat import stop
+from bfp_unicornphat import stripe_horizontally_reverse
+
+########################################################################
+#                         Import variables                             #
+########################################################################
+
+from bfp_unicornphat import X_COORDINATES
+from bfp_unicornphat import Y_COORDINATES
+
+########################################################################
+#                            Functions                                 #
+########################################################################
+
+
+def horizontal_striper_11():
+    """
+    Gets x and y coordinates and sends them to the striping function
+
+    This version reverses the order of the x coordinates.
+    """
+
+    x_coordinate_list = X_COORDINATES[::-1]
+    y_coordinate_list = Y_COORDINATES
+
+    stripe_horizontally_reverse(x_coordinate_list, y_coordinate_list)
+
+
+if __name__ == '__main__':
+    try:
+        # STEP01: Print header
+        print_header()
+        # STEP02: Print instructions in white text
+        print("\033[1;37;40mPress Ctrl-C to stop the program.")
+        # STEP03:
+        horizontal_striper_11()
+        # STEP04: Exit the program.
+        stop()
+    except KeyboardInterrupt:
+        stop()
