fe65f91fb817cd3752bc03ba7b65b7056cfc2266
Paul <13591438+Breakfast-for-Pigeons@users.noreply.github.com>
2018-10-08 11:55:52 -0500

    Add files via upload

---
+++ Pibow_Zero_1.3/moving_diagonal_rainbow_4.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""
+Moving Diagonal Rainbow 4 - Pibow Zero 1.3
+
+Retrieves the rainbows and sends them to the move function.
+With the GPIO pins at the top of the Raspberry Pi, the rainbows move
+from the bottom right the top left.
+
+....................
+
+Functions:
+- moving_diagonal_rainbow_4: Retrieves the rainbows and sends them to
+      the move function.
+
+....................
+
+Author: Paul Ryan
+
+This program was written on a Raspberry Pi using the Geany IDE.
+"""
+########################################################################
+#                          Import modules                              #
+########################################################################
+
+from bfp_unicornphat import print_header
+from bfp_unicornphat import stop
+from bfp_unicornphat import get_diagonal_rainbows_2
+from bfp_unicornphat import move_diagonally
+
+########################################################################
+#                            Functions                                 #
+########################################################################
+
+
+def moving_diagonal_rainbow_4():
+    """
+    Gets 4 rainbows, assigns them in reverse order, and sends them to
+    the move_diagonally function
+    """
+
+    rainbow03, rainbow02, rainbow01, \
+        rainbow00 = get_diagonal_rainbows_2()
+
+    move_diagonally(rainbow00, rainbow01, rainbow02, rainbow03)
+
+
+if __name__ == '__main__':
+    try:
+        # STEP01: Print header
+        print_header()
+        # STEP02: Print instructions in white text
+        print("\033[1;37;40mPress Ctrl-C to stop the program.")
+        # STEP03:
+        moving_diagonal_rainbow_4()
+        # STEP04: Exit the program.
+        stop()
+    except KeyboardInterrupt:
+        stop()
---
+++ Pibow_Zero_1.3/moving_horizontal_rainbow_1.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""
+Moving Horizontal Rainbow 1 - Pibow Zero 1.3
+
+Retrieves the rainbows and sends them to the move function.
+With the GPIO pins at the top of the Raspberry Pi, the rainbows move
+from left to right.
+
+....................
+
+Functions:
+- moving_horizontal_rainbow_1: Retrieves the rainbows and sends them to
+      the move function.
+
+....................
+
+Author: Paul Ryan
+
+This program was written on a Raspberry Pi using the Geany IDE.
+"""
+########################################################################
+#                          Import modules                              #
+########################################################################
+
+from bfp_unicornphat import print_header
+from bfp_unicornphat import stop
+from bfp_unicornphat import get_horizontal_rainbow_00
+from bfp_unicornphat import get_horizontal_rainbows
+from bfp_unicornphat import move_horizontally
+
+########################################################################
+#                            Functions                                 #
+########################################################################
+
+
+def moving_horizontal_rainbow_1():
+    """
+    Retrieves the rainbows and sends them to the move function.
+    """
+
+    rainbow00 = get_horizontal_rainbow_00()
+
+    rainbow01, rainbow02, rainbow03 = get_horizontal_rainbows()
+
+    move_horizontally(rainbow00, rainbow01, rainbow02, rainbow03)
+
+
+if __name__ == '__main__':
+    try:
+        # STEP01: Print header
+        print_header()
+        # STEP02: Print instructions in white text
+        print("\033[1;37;40mPress Ctrl-C to stop the program.")
+        # STEP03:
+        moving_horizontal_rainbow_1()
+        # STEP04: Exit the program.
+        stop()
+    except KeyboardInterrupt:
+        stop()
