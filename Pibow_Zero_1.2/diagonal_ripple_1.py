2190b5bbaf9d62fbf10d9e107469674426169c9e
Paul <13591438+Breakfast-for-Pigeons@users.noreply.github.com>
2018-10-10 13:49:32 -0500

    Update diagonal_ripple_1.py

--- Pibow_Zero_1.2/diagonal_ripple_1.py
+++ Pibow_Zero_1.2/diagonal_ripple_1.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """
-Diagonal Ripple 2 - Pibow Zero 1.2
+Diagonal Ripple 1 - Pibow Zero 1.2
 
 Retrieves the rainbows and sends them to the ripple function.
 With the GPIO pins at the top of the Raspberry Pi, the rainbows ripple
-from the bottom left to the top right.
+from the top right to the bottom left.
 
 ....................
 
 Functions:
-- diagonal_ripple_2: Gets 12 rainbows and sends them to the ripple
+- diagonal_ripple_1: Gets 12 rainbows and sends them to the ripple
       diagonally function
 
 ....................
@@ -28,28 +28,28 @@ from bfp_unicornphat import get_horizontal_rainbow_00
 from bfp_unicornphat import get_diagonal_ripple_rainbows_1
 from bfp_unicornphat import ripple_diagonally
 
+
 ########################################################################
 #                            Functions                                 #
 ########################################################################
 
 
-def diagonal_ripple_2():
+def diagonal_ripple_1():
     """
-    Gets 12 rainbows, but assigns them in reverse order, and sends them
-    to the ripple diagonally function
+    Gets 12 rainbows and sends them to the ripple diagonally function
     """
 
     rainbow00 = get_horizontal_rainbow_00()
 
-    rainbow11, rainbow10, rainbow09, rainbow08, rainbow07, rainbow06, \
-        rainbow05, rainbow04, rainbow03, rainbow02, \
-        rainbow01 = get_diagonal_ripple_rainbows_1()
+    rainbow01, rainbow02, rainbow03, rainbow04, rainbow05, rainbow06, \
+        rainbow07, rainbow08, rainbow09, rainbow10, \
+        rainbow11 = get_diagonal_ripple_rainbows_1()
 
-    diagonal_rainbows_2 = [rainbow01, rainbow02, rainbow03, rainbow04,
+    diagonal_rainbows_1 = [rainbow01, rainbow02, rainbow03, rainbow04,
                            rainbow05, rainbow06, rainbow07, rainbow08,
                            rainbow09, rainbow10, rainbow11]
 
-    ripple_diagonally(rainbow00, diagonal_rainbows_2)
+    ripple_diagonally(rainbow00, diagonal_rainbows_1)
 
 
 if __name__ == '__main__':
@@ -59,7 +59,7 @@ if __name__ == '__main__':
         # STEP02: Print instructions in white text
         print("\033[1;37;40mPress Ctrl-C to stop the program.")
         # STEP03:
-        diagonal_ripple_2()
+        diagonal_ripple_1()
         # STEP04: Exit the program.
         stop()
     except KeyboardInterrupt:
